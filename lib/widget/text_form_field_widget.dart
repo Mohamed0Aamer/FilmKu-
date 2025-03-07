2c82e25f687d78181de71c2401f53c692a71bdfc
Muhammad Tamer_275 <109910762+muhammed-idris@users.noreply.github.com>
2025-03-07 04:07:38 +0200

    Update text_form_field_widget.dart

--- lib/widget/text_form_field_widget.dart
+++ lib/widget/text_form_field_widget.dart
@@ -1,42 +1,62 @@
 import 'package:flutter/material.dart';
 
 class TextFormFieldWidget extends StatelessWidget {
+  final EdgeInsetsGeometry padding;
   TextFormFieldWidget({
     super.key,
-    required this.controller,
+    // required this.controller,
     required this.hintText,
-    required this.icon,
+    // required this.icon,
+    required this.padding
   });
-  TextEditingController controller;
+  // TextEditingController controller;
   String hintText;
-  IconData icon;
-  //String Function(String?)? validator;
+  // IconData icon;
+  // String Function(String?)? validator;
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      style: TextStyle(color: Colors.white, fontSize: 20),
-      decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.bold,
-          color: const Color.fromARGB(124, 255, 255, 255),
+    return Padding(
+      padding: padding,
+      child: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(139, 13, 136, 236),
+          borderRadius: BorderRadius.circular(30),
         ),
-        suffixIcon: Icon(
-          icon,
-          color: Colors.white,
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 17,
+                  letterSpacing: -0.41
+                ),
+                decoration: InputDecoration(
+                  hintText: hintText,
+                  contentPadding: EdgeInsets.only(left: 20),
+                  border: InputBorder.none,
+                  isDense: true,
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    letterSpacing: -0.41
+                  )
+                ),
+              ),
+            ),
+          ],
         ),
-        focusedBorder: _outlineinputborder(Colors.blue),
-        enabledBorder: _outlineinputborder(Colors.white),
       ),
     );
+    // TextFormField(
+
   }
 
-  OutlineInputBorder _outlineinputborder(Color color) {
-    return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(30),
-      borderSide: BorderSide(color: color, width: 3),
-    );
-  }
+//   OutlineInputBorder _outlineinputborder(Color color) {
+//     return OutlineInputBorder(
+//       borderRadius: BorderRadius.circular(30),
+//       borderSide: BorderSide(color: color, width: 3),
+//     );
+//   }
 }
